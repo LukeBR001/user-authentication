1f94039d2535f0de622a3da904793df8fd9ae9fd
Lucas Eduardo <lucas.eduardo@m4u.com.br>
2023-08-01 20:51:52 -0300

    alter initial DDL migration

--- src/main/resources/db/migration/V1__DDL_create_table_users.sql
+++ src/main/resources/db/migration/V1__DDL_create_table_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id int PRIMARY KEY UNIQUE NOT NULL,
     username varchar(255) UNIQUE NOT NULL,
-    password varchar(11) NOT NULL,
-    status TEXT NOT NULL,
-    role TEXT NOT NULL
+    password varchar(255) NOT NULL,
+    status varchar NOT NULL,
+    role varchar NOT NULL
 );
